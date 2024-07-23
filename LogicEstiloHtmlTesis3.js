f7b8eeb550e6f503097d7295b7960df52b01b389
hdmirandaingsi <mg23035@ues.edu.sv>
2024-07-22 18:31:05 -0600

    Add files via upload
    un pasito mas

---
+++ LogicEstiloHtmlTesis3.js
@@ -0,0 +1,15 @@
+#math - formula {
+    /* Estilos para la fórmula completa */
+}
+
+#matrix {
+    display: grid;
+    grid - template - columns: 1.5em 1.5em 1.5em 1.5em;
+    grid - template - rows: 1.5em 1.5em 1.5em 1.5em;
+    justify - items: center;
+    align - items: center;
+}
+#matrix > mrow {
+    display: block;
+    width: 4.5em;
+}
